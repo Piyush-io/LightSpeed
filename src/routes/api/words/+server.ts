e3dfaee8b55ceba44fe9e919bb74c21ff050d060
Piyush Mittal <mittalpiyush2004@gmail.com>
2024-06-17 16:13:55 +0530

    changed path for json

--- src/routes/api/words/+server.ts
+++ src/routes/api/words/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 
-import english from '/Users/xhail/Desktop/Github/Something that sounds interesting/src/routes/api/words/english/english.json'
+import english from '/Users/xhail/Desktop/Github/LightSpeed/src/routes/api/words/english/english.json'
 
 export const GET: RequestHandler = ({ url }) => {
   const limit = Number(url.searchParams.get('limit'))
